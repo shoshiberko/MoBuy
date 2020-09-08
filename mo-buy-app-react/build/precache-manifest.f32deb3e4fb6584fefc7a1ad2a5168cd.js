e3f01ebd8b4aec39ba51d9d9802ae3a9bbb15406
talattar1234 <attar.tal@gmail.com>
2020-09-08 20:13:09 +0300

    build cra

---
+++ mo-buy-app-react/build/precache-manifest.f32deb3e4fb6584fefc7a1ad2a5168cd.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eadd6c333015325e5aeb88462d3f44b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe2e1af0af389fe55818",
+    "url": "/static/css/2.491e0341.chunk.css"
+  },
+  {
+    "revision": "01f04816ecf5a99e6a77",
+    "url": "/static/css/main.0372cfb4.chunk.css"
+  },
+  {
+    "revision": "fe2e1af0af389fe55818",
+    "url": "/static/js/2.c3713d25.chunk.js"
+  },
+  {
+    "revision": "e61de2e42d35c8ebf424e380da9657dd",
+    "url": "/static/js/2.c3713d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f04816ecf5a99e6a77",
+    "url": "/static/js/main.fcccefbc.chunk.js"
+  },
+  {
+    "revision": "312e0b2d3017a2100e50",
+    "url": "/static/js/runtime-main.6162c7a9.js"
+  },
+  {
+    "revision": "3a8c0d8be7fe574d48f58646c2915662",
+    "url": "/static/media/contact5.3a8c0d8b.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "de47ec27ee09a328b5add6c55ca7381b",
+    "url": "/static/media/phone_background1.de47ec27.jpg"
+  },
+  {
+    "revision": "3407d5c3dbe6195f7214203c848362c2",
+    "url": "/static/media/phone_background12.3407d5c3.jpg"
+  },
+  {
+    "revision": "2ad79105aca0f360a5e067fa425fe7a0",
+    "url": "/static/media/phone_background13.2ad79105.jpg"
+  },
+  {
+    "revision": "b2f39b71d911f817258a4ebaafcc4424",
+    "url": "/static/media/phone_background14.b2f39b71.jpg"
+  },
+  {
+    "revision": "11ae6837bb176c1a714afd39e364ddbf",
+    "url": "/static/media/phone_background2.11ae6837.jpg"
+  },
+  {
+    "revision": "3f81a805464f476f05325564b7a3d1af",
+    "url": "/static/media/phone_background3.3f81a805.jpg"
+  },
+  {
+    "revision": "56052cd82d8b625bde45ea2407a7cf38",
+    "url": "/static/media/phone_background4.56052cd8.jpg"
+  },
+  {
+    "revision": "11e0c2ba7d6d66d55af06382fd7b01d4",
+    "url": "/static/media/phone_background5.11e0c2ba.jpg"
+  },
+  {
+    "revision": "b8c8848408f19edb4ae91236df6fd52b",
+    "url": "/static/media/phone_background6.b8c88484.jpg"
+  },
+  {
+    "revision": "9cc4abf0ae743d44d75e4a08c373a235",
+    "url": "/static/media/phone_background8.9cc4abf0.jpg"
+  },
+  {
+    "revision": "8623f77cca33dd96074660a5495f471e",
+    "url": "/static/media/shop2.8623f77c.jpg"
+  },
+  {
+    "revision": "86274e4e8cfd6e23960642fe6498c20b",
+    "url": "/static/media/shop3.86274e4e.jpg"
+  },
+  {
+    "revision": "3f094242471dc40603ba967ab5f18e94",
+    "url": "/static/media/shop4.3f094242.jpg"
+  },
+  {
+    "revision": "8fe5ec53b7087fd022b86979f54e9e35",
+    "url": "/static/media/shop5.8fe5ec53.jpg"
+  },
+  {
+    "revision": "a4f802ac65082a21da7221e986ad077e",
+    "url": "/static/media/shop7.a4f802ac.jpg"
+  },
+  {
+    "revision": "83cb718025f0d91985be6733d1762e5f",
+    "url": "/static/media/shop8.83cb7180.jpg"
+  }
+]);
